3241d88cb5cf5eacc85adc97d54c5f9114eb2a3d
RashmiDodamani <rashmi.dodamani@gmail.com>
2024-10-05 09:49:22 +0530

    done

--- prog1.c
+++ prog1.c
@@ -3,4 +3,5 @@
 printf("this is line 2 in master");
 printf("this is line 1 in featurebranch")
 printf("this is the line i have written before stashing");
-}
+printf("this line i am writting befor stash");
+printf("push");}
